5e444847c90f4e6f20824ae3b7d09504f9842e88
SzymanskiT5 <79137973+SzymanskiT5@users.noreply.github.com>
2021-04-20 18:21:22 +0200

    Working on join tables, calendar added

--- app/main_app.py
+++ app/main_app.py
@@ -2,6 +2,8 @@ from flask import render_template, redirect, url_for, Blueprint, request, sessio
 from . import db
 from .uservalidator import UserValidator
 from typing import Union
+from .models import Training
+
 
 main_blueprint = Blueprint('main', __name__)
 login_blueprint = Blueprint('login', __name__)
@@ -9,6 +11,17 @@ my_schedule_blueprint = Blueprint('myschedule', __name__)
 sign_up_blueprint = Blueprint("signup", __name__)
 logout_blueprint = Blueprint("logout",__name__)
 
+events = [
+    {
+        "name":" test",
+        "date": "2021-04-20",
+
+
+
+    }
+
+
+]
 
 def check_if_logged_in(template_name: str):
     if "nick" in session:
@@ -18,10 +31,9 @@ def check_if_logged_in(template_name: str):
     return render_template(template_name)
 
 
-
 def check_if_logged_myschedule() -> Union[Response, str]:
     if "nick" in session:
-        return render_template("myschedule.html")
+        return render_template("myschedule.html", events =events)
     flash("You need to log in to check the schedule", 'warning')
     return redirect(url_for("login.login"))
 
@@ -57,7 +69,17 @@ def login():
 @my_schedule_blueprint.route('/myschedule', methods=["POST", "GET"])
 def my_schedule():
     if request.method == "POST":
-        pass
+        name = request.form['name']
+        date = request.form['date']
+        duration = request.form['duration']
+        note = request.form['note']
+        rate = request.form['rate']
+        training = Training(name=name, date=date, duration=duration, note=note, rate=rate, user=session["nick"])
+        db.session.add(training)
+        db.session.commit()
+        flash("Training added!", "success")
+        return render_template("myschedule.html", events = events)
+
 
 
     elif request.method == "GET" :
--- app/models.py
+++ app/models.py
@@ -8,6 +8,8 @@ class Users(db.Model):
     name = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(100), unique=True)
     password_hash = db.Column(db.String(128))
+    workouts = db.relationship("Training", backref="user")
+
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -22,15 +24,22 @@ class Training(db.Model):
     date = db.Column(db.DateTime, nullable=False)
     duration = db.Column(db.Integer, nullable=False)
     note = db.Column(db.String(length=5000), nullable=True)
+    rate = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users._id"))
 
-    def __init__(self, name: str, date: datetime, duration: int, note: str) -> None:
-        self.name = name
-        self.date = date
-        self.duration = duration
-        self.note = note
 
-    def update(self, modified_training: Training) -> None:
-        self.name = modified_training.name
-        self.date = modified_training.date
-        self.duration = modified_training.duration
-        self.note = modified_training.note
+    # def __init__(self, name: str, date: datetime, duration: int, note: str, rate: int, user_id:int) -> None:
+    #     self.name = name
+    #     self.date = date
+    #     self.duration = duration
+    #     self.note = note
+    #     self.rate = rate
+    #     self.user_id = user_id
+    #
+    # def update(self, modified_training: Training) -> None:
+    #     self.name = modified_training.name
+    #     self.date = modified_training.date
+    #     self.duration = modified_training.duration
+    #     self.note = modified_training.note
+    #     self.rate = modified_training.rate
+
